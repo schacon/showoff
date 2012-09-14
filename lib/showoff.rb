7cab96716aafdd760ee25953617cf6a39f779ea8
Michael Hackett <michael.hackett@dal.ca>
2012-09-13 21:23:14 -0300

    Parse rendered slide content as HTML instead of XML, to preserve HTML entities, which are removed by Nokogiri's XML parser.

--- lib/showoff.rb
+++ lib/showoff.rb
@@ -297,7 +297,7 @@ class ShowOff < Sinatra::Application
     end
 
     def update_commandline_code(slide)
-      html = Nokogiri::XML.parse(slide)
+      html = Nokogiri::HTML.fragment(slide)
       parser = CommandlineParser.new
 
       html.css('pre').each do |pre|
@@ -339,7 +339,7 @@ class ShowOff < Sinatra::Application
         end
         transform.apply(tree)
       end
-      html.root.to_s
+      html.to_html
     end
 
     def get_slides_html(static=false, pdf=false)
