0e27576ba43522e07191218d4d5e30f67d974769
Franklin Webber <franklin.webber@gmail.com>
2013-03-04 00:46:10 -0700

    Simplified and removed the .sh_ prefix for code execution

--- lib/parade/renderers/html_with_pygments.rb
+++ lib/parade/renderers/html_with_pygments.rb
@@ -7,41 +7,6 @@ module Parade
     class HTMLwithPygments < Redcarpet::Render::XHTML
       include Rouge::Plugins::Redcarpet
 
-      #
-      # When rendering the markdown, the code should be rendered using the
-      # Pygments highlight which corresponds to the ghf_marked.css
-      #
-      # Additionally a class `sh_javascript` or `sh_ruby` is added that will
-      # assist in providing a system to provide live interactive elements
-      # through the javascript defined in `parade.js`.
-      #
-      # @param [String] code the fenced code to be highlighted
-      # @param [String] language the name of the fenced code
-      #
-      # def block_code(code, language)
-      #   syntax_highlighted_html = Pygments.highlight code, :lexer => language,
-      #     :options => {:encoding => 'utf-8'}
-
-      #   syntax_highlighted_html.gsub('class="highlight"',"class=\"highlight sh_#{language}\"")
-      # end
-
-      def block_code(code, language)
-        lexer = Rouge::Lexer.find_fancy(language, code) || Rouge::Lexers::Text
-
-        # XXX HACK: Redcarpet strips hard tabs out of code blocks,
-        # so we assume you're not using leading spaces that aren't tabs,
-        # and just replace them here.
-        if lexer.tag == 'make'
-          code.gsub! /^    /, "\t"
-        end
-
-        formatter = Rouge::Formatters::HTML.new(
-          :css_class => "highlight sh_#{lexer.tag}"
-        )
-
-        formatter.format(lexer.lex(code))
-      end
-
       def self.render(content)
         markdown = Redcarpet::Markdown.new(Renderers::HTMLwithPygments,
           :fenced_code_blocks => true,
